d2c4a0d20827a0cda5d8b1ea1f1a9e2ca3132057
Sahil <sahilflash123@gmail.com>
2021-03-16 22:34:33 +0530

    Added Graphs already made for other project (Graph Adjacency List)

---
+++ Graphs/graph.h
@@ -0,0 +1,41 @@
+/* 27/11/2020
+All Graph functions prototype
+and various typedefs */
+
+#ifndef Gheader
+#define Gheader
+
+//graph pointer
+typedef struct graph* Graph;
+
+typedef struct edges* Edges;
+
+//function to get a pointer to newly created graph with N vertices
+Graph New_Graph(int n); //tested
+
+//function to insert new vertex() with vertex number and 
+//weight i.e is the length of the edge connecting to 
+void insert_edge(Graph graph, int vertex1, int vertex2, double weight); //tested
+
+//returns the size of the adjacency list
+int get_size(Graph g);
+
+//function to remove a vertex and it's respective edges
+void delete_vertex(Graph , int vertex );
+
+//function to remove a specif edge from the graph
+void delete_edge(Graph graph, int vertex1 , int vertex2 );
+
+//get weight
+double get_weight(Graph graph, int vertex1, int vertex2);//tested
+
+//get edges
+Edges get_Edges(Graph graph);//partially tested
+
+//get edgesCount 
+int get_Edges_count(Graph graph); //tested 
+
+//free function
+void free_graph(Graph graph); //tested
+
+#endif
---
+++ Graphs/testgraph.c
@@ -0,0 +1,78 @@
+/* Binary Seacrh tree test functions file
+Date -  12-09-20
+Made by Sahil */
+
+#include <stdio.h>
+#include <assert.h>
+#include "graph.h"
+
+void test_New_Graph();
+void test_insert_edge();
+void test_get_size();
+void test_delete_vertex();
+void test_delete_edge();
+void test_get_weight();
+void test_get_Edges();
+void test_get_Edges_count();
+void test_free_graph();
+
+int main()
+{
+    test_New_Graph();
+    test_insert_edge();
+    test_get_size();
+    test_delete_vertex();
+    test_delete_edge();
+    test_get_weight();
+    test_get_Edges();
+    test_get_Edges_count();
+    test_free_graph();
+        
+
+    printf("All Test Case Passed You are awesome !!!! \n");
+}
+
+void test_New_Graph()
+{
+    //todo
+}
+
+void test_insert_edge()
+{
+    //todo
+}
+
+void test_get_size()
+{
+    //todo
+}
+
+void test_delete_vertex()
+{
+    //todo
+}
+
+void test_delete_edge()
+{
+    //todo
+}
+
+void test_get_weight()
+{
+    //todo
+}
+
+void test_get_Edges()
+{
+    //todo
+}
+
+void test_get_Edges_count()
+{
+    //todo
+}
+
+void test_free_graph()
+{
+    //todo
+}
